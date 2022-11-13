d78e397f45c16d2b0da47de33902fa6b7f1691b2
Edward Peña <58954908+edwardp22@users.noreply.github.com>
2022-11-13 14:48:13 -0800

    add about page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-import { Container } from "../common";
-
 export default function About() {
-  return <Container noFluid>About</Container>;
+  return <div>About</div>;
 }
