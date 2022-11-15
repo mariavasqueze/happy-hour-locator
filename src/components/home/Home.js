3c7cd1f0c9c69f3d427ca8fbade49daeb64cb665
Maria Vasquez <mariadelmarvze@gmail.com>
2022-11-15 14:34:33 -0800

    Update Home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,18 +5,18 @@ import { Row, Col } from "react-bootstrap";
 import { Container, WhiteCenteredContainer } from "../common";
 
 export default function Home() {
-  return (
-    <Container>
-      <Row>
-        <Col xs={12} sm={5}>
-          Happy hour locator
-        </Col>
-        <Col xs={12} sm={7}>
-          <WhiteCenteredContainer>
-            Sign-in to see locations
-          </WhiteCenteredContainer>
-        </Col>
-      </Row>
-    </Container>
-  );
+	return (
+		<Container>
+			<Row>
+				<Col xs={12} sm={5}>
+					<h1>Happy hour locator</h1>
+				</Col>
+				<Col xs={12} sm={7}>
+					<WhiteCenteredContainer>
+						Sign-in to see locations
+					</WhiteCenteredContainer>
+				</Col>
+			</Row>
+		</Container>
+	);
 }
