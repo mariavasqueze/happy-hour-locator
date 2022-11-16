7e42899f660e0327636d4890ac45fc3e1f2a2e9a
Maria Vasquez <mariadelmarvze@gmail.com>
2022-11-15 17:42:46 -0800

    Styles for my profile fixed

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,20 +1,30 @@
 import React from "react";
+import { Row, Col, Button } from "react-bootstrap";
 
-import { Row, Col } from "react-bootstrap";
+import { Container } from "../common";
+import Signin from "../signin-box/Signin";
 
-import { Container, WhiteCenteredContainer } from "../common";
+import "./style.css";
 
 export default function Home() {
 	return (
 		<Container>
 			<Row>
-				<Col xs={12} sm={5}>
+				<Col className="leftColumn" xs={12} sm={5}>
 					<h1>Happy hour locator</h1>
+					<p>Heloooo</p>
+					<p>Heloooo</p>
+					<Button
+						className="purpleBtn"
+						variant="primary"
+						size="lg"
+						type="submit"
+					>
+						Change Card
+					</Button>
 				</Col>
 				<Col xs={12} sm={7}>
-					<WhiteCenteredContainer>
-						Sign-in to see locations
-					</WhiteCenteredContainer>
+					<Signin></Signin>
 				</Col>
 			</Row>
 		</Container>
