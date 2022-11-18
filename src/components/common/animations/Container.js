623c85142956b609004f32c3f876e2c5760cde13
Edward Peña <58954908+edwardp22@users.noreply.github.com>
2022-11-17 20:04:45 -0800

    changing style of about and faq

--- src/components/common/animations/Container.js
+++ src/components/common/animations/Container.js
@@ -2,13 +2,13 @@ import React from "react";
 
 import { motion } from "framer-motion";
 
-export default function Container({ children, noFluid = false }) {
+export default function Container({ children, noFluid = false, className }) {
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5 }}
-      className={`container${noFluid ? "" : "-fluid"}`}
+      className={`container${noFluid ? "" : "-fluid"} ${className}`}
     >
       {children}
     </motion.div>
